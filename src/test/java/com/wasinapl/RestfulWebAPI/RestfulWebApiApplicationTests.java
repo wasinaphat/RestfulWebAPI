1010fe51933f509193914f64d80ef13b8d3a86b2
wasinaphat <wasinapl@scg.com>
2021-04-25 11:44:26 +0700

    new init

---
+++ src/test/java/com/wasinapl/RestfulWebAPI/RestfulWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wasinapl.RestfulWebAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
